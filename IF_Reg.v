bcfd46c2c61b48c327245e98f77d141c1fbbd0db
Mostafa <mostafa.ebrahimi2002@gmail.com>
2022-11-07 17:50:52 +0330

    IF_Reg: is done(without test)

--- IF_Reg.v
+++ IF_Reg.v
@@ -13,4 +13,15 @@ module IF_Reg(
     input [31:0] pc_in, instruction_in;
     output reg [31:0] pc, instruction;
 
+    always @(posedge clk, posedge rst) begin
+        if (rst)
+            {pc, instruction} <= 64'd0;
+        else if (flush)
+            {pc, instruction} <= 64'd0;
+        else if (freeze)
+            {pc, instruction} <= {pc, instruction};
+        else 
+            {pc, instruction} <= {pc_in, instruction_in};
+    end
+
 endmodule

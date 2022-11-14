fb347371efd331edb039e8636675f777636412dc
Mostafa <mostafa.ebrahimi2002@gmail.com>
2022-11-14 16:45:14 +0330

    IF_Stage.v: is done(without test)

--- IF_Stage.v
+++ IF_Stage.v
@@ -1,5 +1,5 @@
 module IF_Stage(
-    clc,
+    clk,
     rst,
     freeze,
     branch_taken,
@@ -8,9 +8,36 @@ module IF_Stage(
     instruction
 );
 
-    input clc, rst, freeze, branch_taken;
+    input clk, rst, freeze, branch_taken;
     input [31:0] branch_addr;
     output [31:0] pc, instruction;
 
+    wire [31:0] pc_out, adder_out, mux_out;
+
+    PC pc_inst (
+        .clk(clk),
+        .rst(rst),
+        .ld(freeze),
+        .in(mux_out),
+        .out(pc_out)
+    );
+
+    Adder adder_pc_4 (
+        .a(32'd4),
+        .b(pc_out),
+        .out(adder_out)
+    );
+
+    Mux_2_1 mux_2_1_inst (
+        .a(adder_out),
+        .b(branch_addr),
+        .sel(branch_taken),
+        .out(mux_out)
+    );
+
+    Instru_mem inst_mem (
+        .addr(pc_out),
+        .instru(instruction)
+    );
 
 endmodule

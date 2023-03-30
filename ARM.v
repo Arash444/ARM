2f140bd8f7893300df349deab6af18be776c1696
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-03-30 08:32:59 +0330

    ARM.v: make code cleaner

--- ARM.v
+++ ARM.v
@@ -10,30 +10,6 @@ module ARM (
 
     output [31:0] pc_out;
 
-    wire [31:0] 
-        pc_if,
-        pc_if_reg,
-        pc_ID_Reg,
-        pc_exe,
-        pc_exe_reg,
-        pc_mem,
-        pc_mem_reg,
-        pc_wb,
-        WB_value;
-    wire [31:0] 
-        instruction_if,
-        instruction_if_reg,
-        branch_addr,
-        Val_Rn_ID,
-        Val_Rn_ID_Reg,
-        Val_Rm_ID,
-        Val_Rm_ID_Reg;
-    wire [11:0] 
-        shift_operand_ID,
-        shift_operand_ID_Reg;
-    wire [23:0]
-        signed_imm_24_ID,
-        signed_imm_24_ID_Reg;
     wire 
         freeze,
         flush,
@@ -57,7 +33,6 @@ module ARM (
         status_n,
         status_v,
         Two_src;
-        
     wire [3:0] 
         EXE_CMD_ID,
         EXE_CMD_ID_Reg,
@@ -66,20 +41,44 @@ module ARM (
         WB_Dest,
         src1_ID,
         src2_ID;
+    wire [11:0] 
+        shift_operand_ID,
+        shift_operand_ID_Reg;
+    wire [23:0]
+        signed_imm_24_ID,
+        signed_imm_24_ID_Reg;
+    wire [31:0] 
+        pc_if,
+        pc_if_reg,
+        pc_ID_Reg,
+        pc_exe,
+        pc_exe_reg,
+        pc_mem,
+        pc_mem_reg,
+        pc_wb,
+        WB_value,
+        instruction_if,
+        instruction_if_reg,
+        branch_addr,
+        Val_Rn_ID,
+        Val_Rn_ID_Reg,
+        Val_Rm_ID,
+        Val_Rm_ID_Reg;
+        
 
     assign status_z = 1'b0;
     assign status_c = 1'b0;
     assign status_n = 1'b0;
     assign status_v = 1'b0;
+    assign WB_WB_EN = 1'b0;
     assign Hazard = 1'b0;
-    assign freeze = Hazard;
+    assign WB_Dest = 4'b0;
+    assign WB_value = 32'b0;
+    assign branch_addr = 32'b0;
+
     assign branch_taken = B_ID_Reg;
     assign flush = branch_taken;
-    assign branch_addr = 32'b0;
-    assign WB_value = 32'b0;
-    assign WB_WB_EN = 1'b0;
-    assign WB_Dest = 4'b0;
-
+    assign freeze = Hazard;
     assign pc_out = pc_wb;
 
     ///////////////////////////////////////////////////// IF

a37469f2ff573ec9c154ea4942909f76cb346da0
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-04-08 23:16:30 +0330

    change MEM_Reg.v

--- MEM_Reg.v
+++ MEM_Reg.v
@@ -1,19 +1,37 @@
 module MEM_Reg (
     clk,
     rst,
-    pc_in,
-    pc
+    ALU_res_in,
+    Mem_R_EN_in,
+    WB_EN_in,
+    dest_in,
+    Mem_Data_in,
+    
+    ALU_res,
+    Mem_R_EN,
+    WB_EN,
+    dest,
+    Mem_Data
 );
 
-    input clk, rst;
-    input [31:0] pc_in;
-    output reg [31:0] pc;
+    input clk, rst, WB_EN_in, Mem_R_EN_in;
+    input [3:0] dest_in;
+    input [31:0] Mem_Data_in, ALU_res_in;
+
+    output reg WB_EN, Mem_R_EN;
+    output reg [3:0] dest;
+    output reg [31:0] Mem_Data, ALU_res;
 
     always @(posedge clk, posedge rst) begin
         if (rst)
-            pc <= 32'd0;
-        else
-            pc <= pc_in;
-    end
+            {WB_EN, Mem_R_EN, dest, Mem_Data, ALU_res} <= 70'b0;
+        else begin
+            WB_EN <= WB_EN_in;
+            Mem_R_EN <= Mem_R_EN_in;
+            ALU_res <= ALU_res_in;
+            Mem_Data <= Mem_Data_in;
+            dest <= dest_in;
+        end
 
+    end
 endmodule

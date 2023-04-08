2b27277f14ad0bc53f48c76654e0f919ae360042
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-04-08 23:11:17 +0330

    Added EXE_Reg.v

--- EXE_Reg.v
+++ EXE_Reg.v
@@ -1,19 +1,41 @@
 module EXE_Reg (
     clk,
     rst,
-    pc_in,
-    pc
+    ALU_res_in,
+    WB_EN_in,
+    Mem_R_EN_in,
+    Mem_W_EN_in,
+    dest_in,
+    Val_Rm_in,
+
+    ALU_res,
+    Val_Rm,
+    WB_EN,
+    Mem_R_EN,
+    Mem_W_EN,
+    dest
 );
 
-    input clk, rst;
-    input [31:0] pc_in;
-    output reg [31:0] pc;
+    input clk, rst, WB_EN_in, Mem_R_EN_in, Mem_W_EN_in;
+    input [3:0] dest_in;
+    input [31:0] Val_Rm_in, ALU_res_in;
+
+    output reg WB_EN, Mem_R_EN, Mem_W_EN;
+    output reg [3:0] dest;
+    output reg [31:0] Val_Rm, ALU_res;
 
     always @(posedge clk, posedge rst) begin
         if (rst)
-            pc <= 32'd0;
-        else
-            pc <= pc_in;
+            {WB_EN, Mem_R_EN, Mem_W_EN, dest, Val_Rm, ALU_res} <= 71'b0;
+        else begin
+            WB_EN <= WB_EN_in;
+            Mem_R_EN <= Mem_R_EN_in;
+            Mem_W_EN <= Mem_W_EN_in;
+            ALU_res <= ALU_res_in;
+            Val_Rm <= Val_Rm_in;
+            dest <= dest_in;
+        end
+
     end
 
 endmodule

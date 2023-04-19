ad8cf672ccf861cde121fd93a1ad7cec111ffb88
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-04-19 15:20:38 +0330

    Change MEM_Stage.v

--- MEM_Stage.v
+++ MEM_Stage.v
@@ -6,13 +6,22 @@ module MEM_Stage (
     Mem_R_EN,
     Mem_W_EN,
 
-    Mem_Data
+    data_mem
 );
     input Mem_R_EN, Mem_W_EN, clk, rst;
     input [31:0] Val_Rm, ALU_res;
+    output [31:0] data_mem;
+    
+    wire [31:0] addr;
+    assign addr = (ALU_res - 32'd1024) >> 2;
 
-    output reg [31:0] Mem_Data;
-
-    assign Mem_Data = 32'b0;
-
+    memory DM(
+        .clk(clk),
+        .rst(rst),
+        .addr(addr),
+        .MEM_W_EN(Mem_W_EN),
+        .MEM_R_EN(Mem_R_EN),
+        .Val_RM(Val_Rm),
+        .data_mem(data_mem)
+    );
 endmodule

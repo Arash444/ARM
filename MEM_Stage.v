a8002755fb6f5320c1eda225d8171d36ec70b4af
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-05-16 16:51:02 +0330

    Add SRAMController in MEM_Stage.v

--- MEM_Stage.v
+++ MEM_Stage.v
@@ -6,22 +6,49 @@ module MEM_Stage (
     Mem_R_EN,
     Mem_W_EN,
 
+    SRAM_data,
+
+    SRAM_WE_N,
+    SRAM_addr,
+    Ready,
     data_mem
 );
     input Mem_R_EN, Mem_W_EN, clk, rst;
     input [31:0] Val_Rm, ALU_res;
+
+    inout [15:0] SRAM_data;
+
+    output 
+        SRAM_WE_N,
+        Ready;
+
+    output [17:0] SRAM_addr;
     output [31:0] data_mem;
+
     
     wire [31:0] addr;
     assign addr = (ALU_res - 32'd1024) >> 1;
 
-    memory DM(
+    SRAMController SC (
         .clk(clk),
         .rst(rst),
-        .addr(addr),
         .MEM_W_EN(Mem_W_EN),
         .MEM_R_EN(Mem_R_EN),
-        .Val_RM(Val_Rm),
-        .data_mem(data_mem)
+        .ALU_res(addr),
+        .ST_Value(Val_Rm),
+        .SRAM_data(SRAM_data),
+        .read_data(data_mem),
+        .SRAM_WE_N(SRAM_WE_N),
+        .addr(SRAM_addr),
+        .Ready(Ready)
     );
+    // memory DM(
+    //     .clk(clk),
+    //     .rst(rst),
+    //     .addr(addr),
+    //     .MEM_W_EN(Mem_W_EN),
+    //     .MEM_R_EN(Mem_R_EN),
+    //     .Val_RM(Val_Rm),
+    //     .data_mem(data_mem)
+    // );
 endmodule

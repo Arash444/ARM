1cd4a54fdb04c2dfd3c86edb89237aa1bf5576c6
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-06-11 19:47:54 +0330

    SRAMController.v: is done

--- SRAMController.v
+++ SRAMController.v
@@ -36,14 +36,14 @@ module SramController(
 
 
     wire [17:0]
-        sramLowAddr, 
-        sramHighAddr, 
-        sramUpLowAddess, 
-        sramUpHighAddess,
-        sramLowAddrWrite,
-        sramHighAddrWrite;
+        sram_low_addr, 
+        sram_high_addr, 
+        sram_up_low_addess, 
+        sram_up_high_addess,
+        sram_low_addr_write,
+        sram_high_addr_write;
     wire [31:0]
-        memAddr;
+        Mem_address;
 
     reg [2:0]
         ps,
@@ -51,31 +51,31 @@ module SramController(
     reg [15:0]
         dq;
     
-    assign memAddr = ALU_res - 32'd1024;
-    assign sramLowAddr = {memAddr[18:3], 2'd0};
-    assign sramHighAddr = sramLowAddr + 18'd1;
-    assign sramUpLowAddess = sramLowAddr + 18'd2;
-    assign sramUpHighAddess = sramLowAddr + 18'd3;
-    assign sramLowAddrWrite = {memAddr[18:2], 1'b0};
-    assign sramHighAddrWrite = sramLowAddrWrite + 18'd1;
+    assign Mem_address = ALU_res - 32'd1024;
+    assign sram_low_addr = {Mem_address[18:3], 2'd0};
+    assign sram_high_addr = sram_low_addr + 18'd1;
+    assign sram_up_low_addess = sram_low_addr + 18'd2;
+    assign sram_up_high_addess = sram_low_addr + 18'd3;
+    assign sram_low_addr_write = {Mem_address[18:2], 1'b0};
+    assign sram_high_addr_write = sram_low_addr_write + 18'd1;
     assign SRAM_data = MEM_W_EN ? dq : 16'bz;
 
     localparam
-        Idle = 3'd0, 
+        IDLE = 3'd0, 
         DataLow = 3'd1, 
         DataHigh = 3'd2, 
         DataUpLow = 3'd3, 
         DataUpHigh = 3'd4, 
-        Done = 3'd5;
+        DONE = 3'd5;
 
     always @(ps or MEM_W_EN or MEM_R_EN) begin
         case (ps)
-            Idle: ns = (MEM_W_EN == 1'b1 || MEM_R_EN == 1'b1) ? DataLow : Idle;
+            IDLE: ns = (MEM_W_EN || MEM_R_EN) ? DataLow : IDLE;
             DataLow: ns = DataHigh;
             DataHigh: ns = DataUpLow;
             DataUpLow: ns = DataUpHigh;
-            DataUpHigh: ns = Done;
-            Done: ns = Idle;
+            DataUpHigh: ns = DONE;
+            DONE: ns = IDLE;
         endcase
     end
 
@@ -85,50 +85,50 @@ module SramController(
         Ready = 1'b0;
 
         case (ps)
-            Idle: Ready = ~(MEM_W_EN | MEM_R_EN);
+            IDLE: Ready = ~(MEM_W_EN | MEM_R_EN);
             DataLow: begin
                 SRAM_WE_N = ~MEM_W_EN;
                 if (MEM_R_EN) begin
-                    addr = sramLowAddr;
+                    addr = sram_low_addr;
                     read_data[15:0] <= SRAM_data;
                 end
                 else if (MEM_W_EN) begin
-                    addr = sramLowAddrWrite;
+                    addr = sram_low_addr_write;
                     dq = ST_Value[15:0];
                 end
             end
             DataHigh: begin
                 SRAM_WE_N = ~MEM_W_EN;
                 if (MEM_R_EN) begin
-                    addr = sramHighAddr;
+                    addr = sram_high_addr;
                     read_data[31:16] <= SRAM_data;
                 end
                 else if (MEM_W_EN) begin
-                    addr = sramHighAddrWrite;
+                    addr = sram_high_addr_write;
                     dq = ST_Value[31:16];
                 end
             end
             DataUpLow: begin
                 SRAM_WE_N = 1'b1;
                 if (MEM_R_EN) begin
-                    addr = sramUpLowAddess;
+                    addr = sram_up_low_addess;
                     read_data[47:32] <= SRAM_data;
                 end
             end
             DataUpHigh: begin
                 SRAM_WE_N = 1'b1;
                 if (MEM_R_EN) begin
-                    addr = sramUpHighAddess;
+                    addr = sram_up_high_addess;
                     read_data[63:48] <= SRAM_data;
                 end
             end
-            Done: Ready = 1'b1;
+            DONE: Ready = 1'b1;
         endcase
     end
 
     always @(posedge clk or posedge rst) begin
         if (rst)
-            ps <= Idle;
+            ps <= IDLE;
         else 
             ps <= ns;
     end
